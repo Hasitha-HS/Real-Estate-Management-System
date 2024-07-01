4029e919e078df4f856c9f9f6bd0e4594cdd7b39
Hasitha-HS <93602672+Hasitha-HS@users.noreply.github.com>
2024-07-01 10:18:51 +0530

    Start

---
+++ src/test/java/com/dea/PropertySphere/PropertySphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dea.PropertySphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertySphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
