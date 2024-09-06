2ec4eba53207d1ba348b533ace6aef8ad3d10f0a
Harindi Balasuriya <96609085+Harindi21@users.noreply.github.com>
2024-09-07 00:36:46 +0530

    Add files via upload

---
+++ src/services/TenantService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const REST_API_BASE_URL = 'http://localhost:8080/api/transactions';
+
+export const listTenants = ()=>axios.get(REST_API_BASE_URL);
+
+export const createTenant = (tenant) => axios.post(REST_API_BASE_URL, tenant);
+
+export const getTenant = (tenantId) => axios.get(REST_API_BASE_URL+'/'+tenantId);
+
+export const updateTenant = (tenantId, tenant) => axios.put(REST_API_BASE_URL + '/'+ tenantId, tenant);
+
+export const deleteTenant = (tenantId) => axios.delete(REST_API_BASE_URL + '/' + tenantId);
