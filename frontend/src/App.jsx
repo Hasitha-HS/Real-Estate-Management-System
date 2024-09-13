96d84b7b7745f00797a3423ecf25f3491ed82c6b
Dewni Seneviratne <87601733+DewniSeneviratne@users.noreply.github.com>
2024-09-13 19:48:02 +0530

    Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
+import ListTenantComponent from './components/ListTenantComponent'
+import TenantComponent from './components/TenantComponent'
+
+function App() {
+ 
+
+  return (
+    <>
+     <BrowserRouter>
+        <div id="root">
+          <HeaderComponent/>
+          <div className="main-content">
+            <Routes>
+              {/* //http://localhost:3000 */}
+              <Route path='/' element={<ListTenantComponent />}></Route>
+              {/* //http://localhost:3000/tenants */}
+              <Route path='/tenants' element={<ListTenantComponent/>}></Route>
+              {/* //http://localhost:3000/add-tenant */}
+              <Route path='/add-tenant' element= {<TenantComponent/>}></Route>
+              {/* //http://localhost:3000/edit-tenant/1 */}
+              <Route path='/edit-tenant/:id' element = {<TenantComponent/>}></Route>
+            </Routes>
+          </div>
+          <FooterComponent />
+        </div>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
