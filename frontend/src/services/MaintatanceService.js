4241af7d759c8f776fed9dc5e92bed8f3a571b09
Rusiru Abeyrathna <98391311+rusiruabeyrathna@users.noreply.github.com>
2024-09-14 22:41:23 +0530

    Add files via upload

---
+++ frontend/src/services/MaintatanceService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const REST_API_BASE_URL = 'http://localhost:8080/api/maintenance';
+
+//Return the list of all the tenants
+export const listTenants = ()=>axios.get(REST_API_BASE_URL);
+
+//Create tenant
+export const createTenant = (tenant) => axios.post(REST_API_BASE_URL, tenant);
+
+//Get tenant by ID
+export const getTenant = (tenantId) => axios.get(REST_API_BASE_URL+'/'+tenantId);
+
+//Update tenant
+export const updateTenant = (tenantId, tenant) => axios.put(REST_API_BASE_URL + '/'+ tenantId, tenant);
+
+//DeleteTenant
+export const deleteTenant = (tenantId) => axios.delete(REST_API_BASE_URL + '/' + tenantId);
