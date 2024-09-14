205d8284f857a1fccd2018fea6409354ee9a83f7
Dewni Seneviratne <87601733+DewniSeneviratne@users.noreply.github.com>
2024-09-14 19:30:47 +0530

    Delete FE - Marketing directory

--- FE - Marketing/marketingFE/src/api.js
+++
@@ -1,8 +0,0 @@
-// src/api.js
-import axios from 'axios';
-
-const API_URL = 'http://localhost:8080/api/property'; // Adjust according to your server URL
-
-export const api = axios.create({
-  baseURL: API_URL,
-});
