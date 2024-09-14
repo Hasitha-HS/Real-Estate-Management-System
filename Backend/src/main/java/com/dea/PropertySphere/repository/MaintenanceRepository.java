33b6e8b42c9b3cb2a57b3eb8c18136bbe2e280be
Rusiru Abeyrathna <98391311+rusiruabeyrathna@users.noreply.github.com>
2024-09-14 22:01:02 +0530

    added

---
+++ Backend/src/main/java/com/dea/PropertySphere/repository/MaintenanceRepository.java
@@ -0,0 +1,7 @@
+package com.dea.PropertySphere.repository;
+
+import com.dea.PropertySphere.Model.Maintenance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MaintenanceRepository extends JpaRepository<Maintenance,Long> {
+}
