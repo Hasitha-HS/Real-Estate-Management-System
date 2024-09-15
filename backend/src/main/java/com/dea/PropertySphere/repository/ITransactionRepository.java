3c450e21bb279839a23bc00683c695044ceb016b
Harindi Balasuriya <96609085+Harindi21@users.noreply.github.com>
2024-09-15 13:23:50 +0530

    Add files via upload
    Transaction repository

---
+++ backend/src/main/java/com/dea/PropertySphere/repository/ITransactionRepository.java
@@ -0,0 +1,11 @@
+package com.dea.PropertySphere.repository;
+
+import com.dea.PropertySphere.Model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//extends JpaRepository
+//need to pass two parameters (type of the model, type of the pk)
+//pk, id's type is Long
+public interface ITransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
