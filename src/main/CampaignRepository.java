1acad7984c1b2a1b53d8ba96fd1808d3a7d7aabd
Chamali Wedage <94621140+ChamaliWedage-99@users.noreply.github.com>
2024-09-15 20:18:23 +0530

    Added the repository file

---
+++ src/main/CampaignRepository.java
@@ -0,0 +1,8 @@
+package com.dea.PropertySphere.repository;
+
+import com.dea.PropertySphere.Model.Campaign;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CampaignRepository extends JpaRepository<Campaign, Long>{
+    
+}
